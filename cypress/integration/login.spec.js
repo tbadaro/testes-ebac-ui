b43bd1264abc2e6f4c098fed183f4c82617fd573
Tadeu Badaró <t.badaro@gmail.com>
2022-01-19 14:45:53 -0300

    configuração e testes de login

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+context('funcionalidade login', () => {
+
+    it('deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('a > .hidden-xs').should('contain', 'Welcome')
+    })
+
+    it('deve exibir mensagem de erro ao inserir usuário inválido', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('usuarioErrado')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro:')
+    })
+
+    it('deve exibir mensagem de erro ao inserir senha inválida', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('senhaerrada')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro:')
+    })
+})
