9310c4994ea10102bc539d99c8557da8718ee77b
Tadeu Badaró <t.badaro@gmail.com>
2022-01-19 20:34:04 -0300

    Teste de remoção de item do carrinho adicionado

--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -32,5 +32,23 @@ describe('funcionalidade página de produtos', () => {
 
     })
 
+    it('deve remover o produto ao carrinho', () => {
+        var quantidade = 3
+
+        cy.get('[class="product-block grid"]')
+        .contains('Argus All-Weather Tank')
+        .click()
+        cy.get('.button-variable-item-M').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Argus All-Weather Tank” foram adicionados no seu carrinho.')
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .view-cart').click()
+        cy.get('.remove > .fa').click()
+        cy.get('.woocommerce-message').should('contain', '“Argus All-Weather Tank” removido.')
+    })
+
 
 });
